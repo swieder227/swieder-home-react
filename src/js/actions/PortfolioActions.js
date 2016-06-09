2acbc859d4ba2046acfb562dbe2bd131fe6eceea
Seth <seth_wieder@condenast.com>
2016-06-09 15:19:53 -0400

    add conditional to prevent spamming of case study increment

--- src/js/actions/PortfolioActions.js
+++ src/js/actions/PortfolioActions.js
@@ -1,14 +1,38 @@
 import alt from '../alt';
 
 class PortfolioActions {
+  constructor(){
+    this.last_action_complete = true;
+    this.action_duration = 1000;
+  }
+
   updateCaseStudyActive(index) {
     return index;
   }
   incrementCaseStudyActive() {
-    return true;
+    if(this.isSafeToIncrement()){
+      return true;
+    } else {
+      return undefined;
+    }
   }
   decrementCaseStudyActive() {
-    return true;
+    if(this.isSafeToIncrement()){
+      return true;
+    } else {
+      return undefined;
+    }
+  }
+  isSafeToIncrement() {
+    if(this.last_action_complete){
+      this.last_action_complete = false;
+      setTimeout(()=>{
+        this.last_action_complete = true;
+      }, this.action_duration);
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
